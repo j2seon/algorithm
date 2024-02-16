b8a96e9a9dab0f5e284051d07360925808715534
j2seon <ddj04323@bitbucket.org>
2024-02-16 18:01:36 +0900

    20240216

---
+++ dev/Tree/BinarySearchTree2.mjs
@@ -0,0 +1,147 @@
+import { BinaryTree } from "./binaryTree.mjs";
+
+class BinarySearchTree2 {
+  constructor(rootNode = null) {
+    this.root = rootNode;
+  }
+
+  // 삽입
+  insert(data) {
+    // 처음 값을 넣는 경우
+    if(this.root == null) {
+      this.root = new BinaryTree(data);
+      return;
+    }
+
+    let parentNode = null;
+    let currentNode = this.root;
+
+    // 왼쪽이 노드보다 작은거 오른쪽이 큰거
+    while(currentNode != null) {
+      // 내려가면서 현재노드가 부모노드가 될거고
+      parentNode = currentNode;
+      // 삽입할 데이터보다 노드값이 더 크다면
+      if(currentNode.getData() > data) {
+        currentNode = currentNode.getLeftSubTree(); // 왼쪽이동
+      // 삽입할 데이터보다 노드값이 더 작다면
+      } else if(currentNode.getData() < data) {
+        currentNode = currentNode.getRightSubTree();
+      } else {
+        //같다면 while문을 나간다. -> 삽입하기위해서
+        return;
+      }
+    }
+    // 새로만들어주고
+    let addNewNode = new BinaryTree(data);
+
+    // 부모만 자식을 볼수있는 상황이니 부모노드와 이어준다
+    if(parentNode.getData() > data) { // 부모의 데이터가 더 크면 왼쪽~
+      parentNode.setLeftSubTree(addNewNode);
+    } else {
+      parentNode.setRightSubTree(addNewNode); // 부모의 데이터가 더 작으면 오른쪽~
+    }
+  }
+
+  // 조회
+  select(data) {
+    // 처음시작하면 맨위일테니까~
+    let currentNode = this.root;
+
+    while(currentNode != null) {
+      // 전달받은 데이터와 같을 경우
+      if(currentNode.getData() == data) {
+        return currentNode;
+      } else if(currentNode.getData() > data) {
+        currentNode = currentNode.getLeftSubTree();
+      } else {
+        currentNode = currentNode.getRightSubTree();
+      }
+    }
+    return null; //없음
+  }
+  // 제거
+  remove(data){
+    //모든 노드가 부모노드가 있다고 생각하고 제거하기위해 rootNode의 부모를 만들어준다
+    let tempRootNode = new new BinaryTree(0);
+    let parentNode = tempRootNode;
+    let currentNode = this.root;
+    let deletingNode = currentNode;
+
+    tempRootNode.setRightSubTree(this.root); // 그러고 실제 우리의 루트와 연결해주자~
+
+    // null이 아니고 값을 찾기위해 반복문
+    while(currentNode != null && currentNode.getData() != data) {
+      parentNode = currentNode;
+
+      if(currentNode.getData() > data ){
+        currentNode = currentNode.getLeftSubTree();
+      } else {
+        currentNode = currentNode.getRightSubTree();
+      }
+    }
+
+    // 계속 타고 내려갔는데 값이 없어서 null이되고~ 당연히 데이터도 없겠지?
+    if(currentNode == null) return; 
+
+    deletingNode = currentNode;
+
+    // 1.터미널 노드를 없애는경우
+    if(deletingNode.getLeftSubTree() == null && deletingNode.getRightSubTree() == null) {
+      // 1-1. 부모의 왼쪽에 있을때
+      if(parentNode.getLeftSubTree() == deletingNode) {
+        parentNode.removeLeftSubTree();
+      // 1-2. 부모의 오른쪽에 있을때
+      } else {
+        parentNode.removeRightSubTree();
+      }
+    //2. 자식노드가 하나 있는 경우
+    } else if (deletingNode.getLeftSubTree() == null || deletingNode.getRightSubTree() == null ) {
+      // 자식노드를 담기위한 변수
+      let tempNode;
+      // 2-1. 왼쪽에 있는경우
+      if(deletingNode.getLeftSubTree() != null) {
+        tempNode = deletingNode.getLeftSubTree();
+      // 2-2. 오른쪽에 있는 경우
+      } else {
+        tempNode = deletingNode.getRightSubTree();
+      }
+      // 부모노드에서 temp랑 연곂되어야한다
+      // 부모의 왼쪽 노드가 내가 삭제하려는 노드랑 같으면 
+      if(parentNode.getLeftSubTree() == deletingNode) {
+        parentNode.setLeftSubTree(tempNode);
+      }else {
+        parentNode.setRightSubTree(tempNode);
+      }
+    //3. 자식노드가 두개있는 노드를 제거하는 경우 
+    } else {
+      // 
+
+
+    }
+
+
+
+
+
+  }
+}
+
+
+let binarySearchTree = new BinarySearchTree2();
+binarySearchTree.insert(18);
+binarySearchTree.insert(15);
+binarySearchTree.insert(10);
+binarySearchTree.insert(6);
+binarySearchTree.insert(3);
+binarySearchTree.insert(8);
+binarySearchTree.insert(12);
+binarySearchTree.insert(11);
+binarySearchTree.insert(31);
+binarySearchTree.insert(27);
+binarySearchTree.insert(24);
+binarySearchTree.insert(20);
+binarySearchTree.insert(33);
+binarySearchTree.insert(35);
+binarySearchTree.insert(37);
+binarySearchTree.root.inOrderTraversal(binarySearchTree.root);
+
--- dev/Tree/binarySearchTree.mjs
+++ dev/Tree/binarySearchTree.mjs
@@ -54,7 +54,7 @@ class BinarySearchTree{
 
     remove(targetData) {
       // 루트노드를 제거할경우 사용
-      let tempRootNode = new BinarySearchTree(0);
+      let tempRootNode = new BinaryTree(0);
       let parentNode = tempRootNode;
       let currentNode = this.root;
       let deletingNode = null;
@@ -77,13 +77,13 @@ class BinarySearchTree{
       deletingNode = currentNode;
 
       // 터미널 노드를 제거할 경우
-      if(deletingNode.getLeftSubTree() == null && deletingNode.getRightSubTree()) {
+      if(deletingNode.getLeftSubTree() == null && deletingNode.getRightSubTree() == null) {
         if(parentNode.getLeftSubTree() == deletingNode){
           parentNode.removeLeftSubTree()
         } else {
           parentNode.removeRightSubTree();
         }
-        // 자식노드가 하나있는 노드를 제거할 경우
+      // 자식노드가 하나있는 노드를 제거할 경우
       } else if(deletingNode.getLeftSubTree() == null || deletingNode.getRightSubTree()) {
         let deletedNodeChilde;
         if(deletingNode.getLeftSubTree() != null){ // 왼쪽이 있는 경우
@@ -97,20 +97,39 @@ class BinarySearchTree{
         } else {
           parentNode.setRightSubTree(deletedNodeChilde);
         }
-        // 자식노드가 두개있는 노드를 제거할 경우 
+      // 자식노드가 두개있는 노드를 제거할 경우 
       } else {
-        // 왼쪽노드에서 가장 큰 값을 찾아 제거하 노드의 위치에 놔주면 된다 
-        let replaceNode = deletingNode.getRightSubTree();
+        // 왼쪽 자식노드에서 큰값을 삭제하려는 노드와 대체함
+        let replaceNode = deletingNode.getLeftSubTree();
+        let replaceParentNode = deletingNode;
+
+        while(replaceNode.getRightSubTree() != null){
+          replaceParentNode = replaceNode;
+          replaceNode = replaceNode.getRightSubTree();
+        }
+
+        // 제거한 노드의 값을 리턴
+        let resultData = deletingNode.getData();
+        // 노드끼리 바꾸는 것보다 노드의 값만 체인지 해주자
+        deletingNode.setData(replaceNode.getData());
+
+        // replaceParentNode 의 왼쪽이 대체할 노드라면
+        if(replaceParentNode.getLeftSubTree() == replaceNode) {
+          replaceParentNode.setLeftSubTree(replaceNode.getLeftSubTree());
+        }else {
+        // 바꾸는 노드의 부모노드는 오른쪽에 바꾼노드의 왼쪽에 있던 노드로 바뀌어야한
+          replaceParentNode.setRightSubTree(replaceNode.getLeftSubTree());
+        }
+        
+        deletingNode = replaceNode;
+        deletingNode.setData(resultData);
+
+        //만약 루트 노드가 변경되었을때는 변경된 루트ㄶ드로 설정해야한다
+        if(tempRootNode.getRightSubTree() != this.root) {
+          this.root = tempRootNode.getRightSubTree();
+        }
       }
-
-    
-
-
-
-
-
-
-
+      return deletingNode;
     }
 }
 
@@ -139,3 +158,9 @@ console.log(binarySearchTree.search(6));
 
 console.log("========== Search 1 ==========");
 console.log(binarySearchTree.search(31));
+
+console.log("========== remove 10 ==========");
+console.log(binarySearchTree.remove(10));
+
+console.log("========== remove 후 순회 ==========");
+binarySearchTree.root.inOrderTraversal(binarySearchTree.root);
